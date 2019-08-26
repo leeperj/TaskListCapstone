cd7bc72a9b52b417130d8876fa691e088eb0ec69
unknown <josephleeper@gmail.com>
2019-08-26 08:51:37 -0400

    upload tasklist capstone

---
+++ TaskListCapstone6/Controllers/TaskListController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TaskListCapstone6.Models;
+
+namespace TaskListCapstone6.Controllers
+{
+    
+    public class TaskListController : Controller
+    {
+        private readonly TasklistDbContext _context;
+
+        public TaskListController(TasklistDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult AddTask(TaskList newTask)
+        {
+            AspNetUsers thisUser = _context.AspNetUsers.Where(u => u.UserName == User.Identity.Name).First();
+            newTask.UserId = thisUser.Id;
+            _context.TaskList.Add(newTask);
+            _context.SaveChanges();
+            return RedirectToAction("ListTasks");
+        }
+        [Authorize]
+        public IActionResult ListTasks()
+        {
+            List<TaskList> userTasks = _context.TaskList.ToList();
+            return View(userTasks);
+        }
+
+        public IActionResult DeleteTask(int Id)
+        {
+            TaskList found = _context.TaskList.Find(Id);
+            if (found != null)
+            {
+                _context.TaskList.Remove(found);
+                _context.SaveChanges();
+
+            }
+            return RedirectToAction("ListTasks");
+
+        }
+
+        public IActionResult UpdateTask(int Id)
+        {
+            TaskList found = _context.TaskList.Find(Id);
+            if (found != null)
+            {
+                found.Completed = "yes";
+
+                _context.Entry(found).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _context.Update(found);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("ListTasks");
+        }
+    }
+}
---
+++ TaskListCapstone6/Models/TaskList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskListCapstone6.Models
+{
+    public partial class TaskList
+    {
+        public string UserId { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Completed { get; set; }
+        public int TaskId { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
